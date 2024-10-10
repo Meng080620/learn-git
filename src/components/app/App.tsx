4e384a31785d9b02131cc7fc80b2fc7558b5ca5c
LY Meng <mengmobile777@gmail.com>
2024-10-11 00:26:35 +0700

    done on nav (Simple foot wepsite)

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,11 +1,30 @@
 import React from "react";
 import "./index.css"
 import DefaultLayout from "../layout/DefaultLayout"
+import Nav from "../nav";
+
 const App: React.FC = () => {
   return (
     <>
       <DefaultLayout>
-        Meng
+        {/* Nav */}
+        <Nav />
+        {/* End of Nav */}
+
+        {/* Hero */}
+        {/* End of Hero */}
+
+        {/* About */}
+        {/* End of About */}
+
+        {/* Menu */}
+        {/* End of Menu */}
+
+        {/* Contact */}
+        {/* End of Contact */}
+
+        {/* Footer */}
+        {/* End of Footer */}
       </DefaultLayout>
     </>
   );
